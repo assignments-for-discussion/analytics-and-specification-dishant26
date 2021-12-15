c468bd2aef3286accaf6e0f18b3f01fb43b06788
Dishant Padalia <64226014+dishant26@users.noreply.github.com>
2021-12-15 21:28:58 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,11 +1,11 @@
 
-function removeNaN(numbers) {
+function removenan(numbers) {
   return numbers.filter(value => !Number.isNaN(value));
   
 }
 
 function average(numbers) {
-  const numbers_new = removeNaN(numbers);
+  const numbers_new = removenan(numbers);
   return numbers_new.reduce((p, c)=> p + c, 0) / numbers_new.length;
 }
 
